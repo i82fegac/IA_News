4ec99b47d5cad3482954b45fe2e2d67d7306941b
i82fegac <cesarfergar8@gmail.com>
2023-04-27 20:04:59 +0200

    Segunda Api Implementada.Correción de errores.

--- News_IA/foros/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_temas, name='lista_temas'),
-    path('crear/', views.crear_tema, name='crear_tema'),
-    path('<int:tema_id>/', views.detalle_tema, name='detalle_tema'),
-    path('<int:tema_id>/comentar/', views.crear_comentario, name='crear_comentario'),
-]
